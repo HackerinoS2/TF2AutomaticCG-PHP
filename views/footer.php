be9c7d995a2b4caca7700a9d67169252c819d887
Rui Oliveira <39498755+HackerinoS2@users.noreply.github.com>
2019-05-23 15:11:44 +0100

    Added Footer

--- views/footer.php
+++ views/footer.php
@@ -1,4 +1,20 @@
 <footer>
+    <div id="footer-top" class="bg-dark">
+        <span id="contribuite">
+            <i class="far fa-heart"></i> You want to donate? Here you have my <a
+                href="https://steamcommunity.com/tradeoffer/new/?partner=266806812&token=2kLhDrmz"
+                target="_blank">Steam Trade Offer Link.</a>
+                <br /><br />
+
+                <i class="fab fa-github"></i> Want to colaborate with the project? Check the tool's <a href="#" target="_blank">GitHub Page!</a>
+                <br /><br />
+        </span>
+
+        <br /><br /><br /><br />
+
+        <span id="madeby">Tool made with <i class="far fa-heart"></i> by: <a href="http://steamcommunity.com/profiles/76561198227072540"
+                target="_blank">Hackerino</a></span>
+    </div>
     <!-- JS -->
     <script src="./js/autopricer.js"></script>
 </footer>
--- views/header.php
+++ views/header.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    <!-- META TAGS -->
+  <!-- META TAGS -->
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
   <meta name="description" content="TF2Automatic-CG" />
   <meta name="keywords" content="TF2Automatic, TF2Automatic-CG" />
@@ -17,8 +17,12 @@
   <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet" />
 
   <!-- BOOTSTRAP -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-    crossorigin="anonymous" />
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
+  
+  <!-- FONT-AWESOME -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
+    integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
 
   <!-- WEBPAGE TITLE -->
   <title>TF2-Automatic | CG</title>
@@ -28,4 +32,3 @@
 </head>
 
 <body>
-    
