194af26b20ff124847a67726cb0696d3cd70cc26
Rui Oliveira <39498755+HackerinoS2@users.noreply.github.com>
2019-05-27 16:45:59 +0100

    Recoded JS to use jQuery

--- js/autopricer.js
+++ js/autopricer.js
@@ -1,4 +1,4 @@
-function autopricer() {
+/* function autopricer() {
     var autoprice = document.getElementById('autopricer').value;
 
     if (autoprice == 'Enable Autopricer') {
@@ -8,4 +8,17 @@ function autopricer() {
         document.getElementById('autoprice_form').style.display = 'block';
         document.getElementById('autopricer').value = 'Enable Autopricer';
     }
+} */
+
+function autopricer() {
+    var autoprice = $('#autopricer').val();
+
+    if (autoprice == 'Disable Autopricer') {
+        $('#autoprice_form').fadeIn().show();
+        $('#autopricer').val('Enable Autopricer');
+    } else if (autoprice == 'Enable Autopricer') {
+        $('#autoprice_form').hide();
+        $('#autopricer').val('Disable Autopricer');
+    }
 }
+
--- views/home.php
+++ views/home.php
@@ -267,9 +267,6 @@
   </center>
 </form>
 
-<!-- JQUERY -->
-<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-
 <script>
   $(document).ready(function () {
     $("#form").submit(function(e) {
