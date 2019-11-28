f84cbf599adc13aa04e33dd6c9c4450555b2b04b
Rui Oliveira <39498755+HackerinoS2@users.noreply.github.com>
2019-11-28 17:53:09 +0000

    Teste...

--- controllers/processData.inc.php
+++ controllers/processData.inc.php
@@ -4,6 +4,7 @@ class ProcessData {
     //Properties
     public $itemsArray; 
     public $quality;
+    public $qualityList = ['Unique','Strange','Vintage','Genuine','Haunted','Unusual'];
 
     //Methods
     function split_items_into_array ($items) {
@@ -11,11 +12,20 @@ class ProcessData {
         return $this->itemsArray;
     }
 
-    function quality_to_command ($quality) {
-        switch ($quality) {
-
-            default:
-            $this->quality = '';
+    function convert_quality_to_command ($string) {
+        //CHANGE THIS TO A FOREACH LOOP YOU DUMB DUCK
+        for ($i = 0; $i < sizeof($this->qualityList); $i++) {
+            if (strpos($string, $this->qualityList[$i]) !== false) {
+                //Removes the quality from string
+                $newString = str_ireplace($this->qualityList[$i], "", $string);
+                //Add quality command piece to propertie
+                $this->quality = '&quality='.$this->qualityList[$i];
+                //Returns string without quality
+                return $newString;
+                break;
+            } else {
+                $this->quality = '&quality='.$this->qualityList[0];
+            }
         }
     }
 }
